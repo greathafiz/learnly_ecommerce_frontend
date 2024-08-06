98e26f2d084fdfac6c3fd9df023c3461fe98b350
Abdulhafiz <abdulhafizaderemi@gmail.com>
2024-08-06 23:32:06 +0100

    setup

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { useToast } from "vue-toastification";
+import { onMounted } from "vue";
+
+onMounted(() => {
+  const toast = useToast();
+
+  toast("I am a toast");
+
+  toast.success("My toast content", {
+    timeout: 2000,
+  });
+});
+</script>
+
+<template>
+  <h1 class="text-3xl">Hello World!</h1>
+</template>
